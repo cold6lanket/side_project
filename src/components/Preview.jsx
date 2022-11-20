45afcbade7ea24e395436e9272565347542a6250
Mereke <kz9merek@gmail.com>
2022-11-20 19:45:11 +0600

    changes

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -1,10 +1,34 @@
-import {Fragment} from "react";
+import { Fragment } from "react";
+
+const pStyle = {
+    textIndent: "30px"
+};
+
+const buttonStyle = {
+    backgroundColor: "#0095ff",
+    border: "1px solid transparent",
+    borderRadius: "3px",
+    boxShadow: "rgba(255, 255, 255, .4) 0 1px 0 0 inset",
+    color: "#fff",
+    cursor: "pointer",
+    display: "inline-block",
+    fontSize: "13px",
+    fontWeight: 400,
+    outline: "none",
+    padding: "8px .8em",
+    textAlign: "center",
+    textDecoration: "none"
+};
 
 const contentTypes = {
     headline: (text, styles = {}) => <h1 style={styles}>{text}</h1>,
-    button: (text, styles = {}) => <button style={styles}>{text || "click"}</button>,
-    paragraph: (text, styles = {}) => <p style={styles}>{text}</p>,
+    button: (text, styles = {}) => <button style={{ ...buttonStyle, ...styles }}>{text || "click"}</button>,
+    paragraph: (text, styles = {}) => <p style={{ ...pStyle, ...styles }}>{text}</p>,
     image: (src, styles = {}) => <img style={{ width: 400, height: 200, ...styles }} src={src} alt="" />
+};
+
+const formatters = {
+    paragraph: (text) => text,
 };
 
 export function Preview({ content }) {
